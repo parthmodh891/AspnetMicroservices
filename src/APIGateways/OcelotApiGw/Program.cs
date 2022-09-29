85cc3e27cbd01d7b84eb5949ebbef8e6a592b76b
parth <parthmodh891@gmail.com>
2022-09-29 17:36:25 +0530

    Added API Gateway for Ocelot API Gateway

---
+++ src/APIGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,35 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+using Ocelot.Values;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+    {
+        config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+    })
+    .ConfigureLogging((hostingContext, loggingbuilder) =>
+    {
+        loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        loggingbuilder.AddConsole();
+        loggingbuilder.AddDebug();
+    });
+
+builder.Services.AddOcelot()
+                .AddCacheManager(settings => settings.WithDictionaryHandle());
+
+
+
+
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapGet("/", () => "Hello World!");
+
+await app.UseOcelot();
+
+app.Run();
